05336cf719a900bc1afb9fb55e1511252cee8078
Nitin Misra <nitin.misra84@gmail.com>
2021-07-20 09:02:26 -0700

    lecture ready to go

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const path = require("path");
-const { removeAllListeners } = require("process");
 // require rollbar below
-const Rollbar = require("rollbar");
+
 // create the Rollbar class below
-const rollbar = new Rollbar({
-  accessToken: "d7d8fd3b146641048e35d907606d6d0a",
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
 
 const app = express();
 app.use(express.json());
-app.use(rollbar.errorHandler());
 
 let studentList = [];
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
   // send rollbar some info
-  rollbar.info("html file served successfully");
 });
 
 app.post("/api/student", (req, res) => {
@@ -35,19 +27,15 @@ app.post("/api/student", (req, res) => {
   if (index === -1 && name !== "") {
     studentList.push(name);
     // add rollbar log here
-    rollbar.log("student added succesfully", {
-      author: `${myName}`,
-      type: "manual",
-    });
 
     res.status(200).send(studentList);
   } else if (name === "") {
     // add a rollbar error here
-    rollbar.error("no name given");
+
     res.status(400).send({ error: "no name was provided" });
   } else {
     // add a rollbar error here too
-    rollbar.error("student already exists");
+
     res.status(400).send({ error: "that student already exists" });
   }
 });
